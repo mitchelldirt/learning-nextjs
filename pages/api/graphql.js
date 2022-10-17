4d8856bb8c1bfee39c362e8f75addbc55d5b7ac9
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-17 07:47:00 -0400

    Worked on 6th lesson. Need to update some code because the tutorial is outdated.

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,8 +1,37 @@
-export default (req, res) => {
-	res.status(200).json({
-		test: 'Hello Level Up, but better'
-	})	
+import { ApolloServer, gql, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-micro'
+
+const typeDefs = gql`
+type Query {
+sayHello: String
 }
+`
+
+const resolvers = {
+	Query: {
+		sayHello: () => {
+			return 'Hello Level Up!!'
+		}
+	}
+}
+
+const apolloserver = new ApolloServer({typeDefs, resolvers});
+
+
+export const config = {
+	api: {
+		// Turns off body parser
+		bodyParser: false
+	}
+}
+
+export default apolloserver.createHandler({path: '/api/graphql'})
+
+//EXAMPLE
+//export default (req, res) => {
+//	res.status(200).json({
+//		test: 'Hello Level Up, but better'
+//	})	
+//}
 
 // LONG WAY
 //export default (req, res) => {
